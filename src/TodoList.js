e7dbf29a3d737b70ad127bb79561889c9a6c6c3a
Nick Hamnett <nick@sameoldnick.com>
2023-08-02 11:55:23 -0600

    Added useEffect functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,6 +6,32 @@ const TodoList = ({ tasks }) => {
   // Don't change the state in the same scope as the return!!!
   //setTasks([...tasks, 'Task 4']);
 
+  React.useEffect(() => {
+    console.log('useEffect called');
+
+    const resizeCallback = () => {
+      console.log('resized');
+    }; 
+
+    // DONT DO THIS!
+    //window.onresize = resizeCallback;
+
+    // DO THIS!!!
+    window.addEventListener('resize', resizeCallback);
+
+    const timer = setInterval(() => {
+      //alert('5 secs later');
+    }, 5000);
+
+    // Cleanup function
+    // Not always needed
+    return () => {
+      console.log('unmounted');
+      clearInterval(timer);
+      window.removeEventListener('resize', resizeCallback);
+    }
+  }, [tasks]);
+
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
 
